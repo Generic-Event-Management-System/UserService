cc62be65ffd322e4cd9e51768d1ed4221395a7e6
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-07 21:33:20 +0200

    Added shared library in the project and registered the custom exception handling middleware in the program.cs

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SharedUtilities.Middleware;
 using UserService.Persistence;
 using UserService.Services;
 using UserService.Services.Contracts;
@@ -19,6 +20,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
